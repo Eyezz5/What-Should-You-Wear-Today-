55ba7d9c16738f61fbc8c796fda501fbe8dfd9fe
ihateuu <replacement506@gmail.com>
2021-07-31 23:42:04 +0300

    Initial Commit

---
+++ Unknown/UnknownApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnknownApp.swift
+//  Unknown
+//
+//  Created by Kareem Almomen on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct UnknownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
