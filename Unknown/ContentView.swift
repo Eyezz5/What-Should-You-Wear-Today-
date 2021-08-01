4535d7fcebda5d4381bd697cbdcb27424030a76d
ihateuu <replacement506@gmail.com>
2021-08-01 21:51:37 +0300

    Final

--- Unknown/ContentView.swift
+++ Unknown/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var degrees = ""
+    @State private var imageName = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("What Should You Wear Today?")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding()
+                Spacer()
+            TextField("Enter Temperature Here", text: $degrees)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
+                Spacer()
+                    Spacer()
+                .onChange(of: degrees, perform : { value in
+                    if let value = Int(value) {
+                        if value < 60 {
+                            imageName = "Coat"
+                        }
+                        if value >= 60 {
+                            imageName = "Shirt"
+                        }
+                        if value >= 80 {
+                            imageName = "Towel"
+                        }
+                    }
+                })
+            Image(imageName)
+                .padding()
+        }
     }
 }
 
